0101d074d52b563e8f2999052a1676de861e5c8b
Mohammed Wasfi <mohammedwasfi@Mohammeds-MacBook-Pro.local>
2018-01-31 18:56:58 -0600

    Fix indentations using linters

--- site.js
+++ site.js
@@ -1,17 +1,18 @@
 $('html').removeClass('no-js').addClass('js');
 $('#sign-form').on('submit',
-function(e) {
-  var email = $('#email').val();
-  var valid = /.+@.+\..+/g;
-  var validemail = valid.test(email);
-  var name = $('#name').val();
- e.preventDefault();
- if(!validemail){
-   console.log('Invalid email');
-   alert("The email you entered was invalid");
-   return false;
- } else {
-   console.log('Valid email');
-   $('body').replaceWith('<h1 id="submission"> Thank you ' + name + '!  your sign up has been completed.</h1>');
- }
-});
+  function(e) {
+    var email = $('#email').val();
+    var valid = /.+@.+\..+/g;
+    var validemail = valid.test(email);
+    var name = $('#name').val();
+    e.preventDefault();
+    if(!validemail){
+      console.log('Invalid email');
+      alert("The email you entered was invalid");
+      return false;
+    }
+    else {
+      console.log('Valid email');
+      $('body').append('<h1 id="submission"> Thank you ' + name + '!  your sign up has been completed.</h1>');
+    }
+  });
