166942a43366e9d9422fd40dcc525aa69697896b
Mohammed Wasfi <mohammedwasfi@Mohammeds-MacBook-Pro.local>
2018-02-18 22:51:03 -0600

    Fix all errors given by the linters

--- site.js
+++ site.js
@@ -10,17 +10,17 @@ jQuery(function($) {
       pattern: /^[^\s@]+@[^\s@]+$/
     };
     e.preventDefault();
-   if (email.val === '') {
-     console.log('email is empty');
-     $('#invalid').remove();
-     $('#emailadd').append('<li id="invalid">Please enter an email</li>');
-   }
-   else if (!(email.pattern).test(email.val)) {
-     console.log('invalid email');
-     $('#invalid').remove();
-     $('#emailadd').append('<li id="invalid">Please enter a valid email</li>');
-   }
-   else {
+    if (email.val === '') {
+      console.log('email is empty');
+      $('#invalid').remove();
+      $('#emailadd').append('<li id="invalid">Please enter an email</li>');
+    }
+    else if (!(email.pattern).test(email.val)) {
+      console.log('invalid email');
+      $('#invalid').remove();
+      $('#emailadd').append('<li id="invalid">Please enter a valid email</li>');
+    }
+    else {
       $(this).remove();
       $('#signup').append('<h1 id="submitted"> Thank you ' + name + '!  your sign up has been completed.</h1>');
     }
